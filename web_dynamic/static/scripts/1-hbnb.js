27cde2db3c25a0dc7db469636bac31e8769c890c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-26 18:05:36 -0500

    added files 1-hbnb

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,9 @@
+$('input[type="checkbox"]').click(function(){
+    if($(this).prop("checked") == true){
+        console.log("Checkbox is checked.");
+    }
+    else if($(this).prop("checked") == false){
+        console.log("Checkbox is unchecked.");
+    }
+});
+
