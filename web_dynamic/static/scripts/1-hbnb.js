9acc92752fce57a783e6af1d5a26e16036c52aa1
Luz Perdomo <lilyperdomo92@gmail.com>
2020-09-26 22:35:59 -0500

    fixed logical syntax, semistandard style, added spaces after coma

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,26 +1,24 @@
-let amenitiesList = []
-$('input[type=checkbox]').prop('checked', false);
+$(function(){
+  let amenitiesList = [];
 
-$('input[type="checkbox"]').click(function(){
-    if($(this).prop("checked") == true){
-        amenitiesList.push($(this).attr('data-name'))
-
-        $( "#select" ).text(amenitiesList).css({
-            "width": "100%",
-            "height": "100%",
-            "white-space": "nowrap",
-            "overflow": "hidden",
-            "text-overflow": "ellipsis",
-            "padding-bottom": "16px"
-        });
-
-    } else if($(this).prop("checked") == false){
-        let idx = 0;
-        amenitiesList.forEach((elemt) => {
-            if (elemt == $(this).attr('data-name'))
-                amenitiesList.splice(idx, 1);
-            idx += 1;
-        });
-        $("#select").text(amenitiesList)
+  $('.amenities h4').css({
+    "width": "100%",
+    "height": "100%",
+    "white-space": "nowrap",
+    "overflow": "hidden",
+    "text-overflow": "ellipsis",
+    "padding-bottom": "16px"
+  });
+  $('input[type="checkbox"]').click(function(){
+    const $checkbox = $(this);
+    if($checkbox.prop("checked") == true){
+      amenitiesList.push($checkbox.data('name'));
+    } else {
+      amenitiesList.forEach((elemt, index) => {
+        if (elemt == $checkbox.data('name'))
+          amenitiesList.splice(index, 1);
+      }); 
    }
+   $('.amenities h4').text(amenitiesList.join(", "));
+  });
 });
