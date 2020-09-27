0df39436001464b4bce336428b54c0b4bdab1495
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-26 22:00:11 -0500

    update task two prop h4 done

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,12 +4,23 @@ $('input[type=checkbox]').prop('checked', false);
 $('input[type="checkbox"]').click(function(){
     if($(this).prop("checked") == true){
         amenitiesList.push($(this).attr('data-name'))
-        $("#select").text(amenitiesList)
 
+        $( "#select" ).text(amenitiesList).css({
+            "width": "100%",
+            "height": "100%",
+            "white-space": "nowrap",
+            "overflow": "hidden",
+            "text-overflow": "ellipsis",
+            "padding-bottom": "16px"
+        });
 
     } else if($(this).prop("checked") == false){
-        amenitiesList.pop()
+        let idx = 0;
+        amenitiesList.forEach((elemt) => {
+            if (elemt == $(this).attr('data-name'))
+                amenitiesList.splice(idx, 1);
+            idx += 1;
+        });
         $("#select").text(amenitiesList)
-
    }
 });
